02fbe3ba9ccab7581ffd3e150430ff1cf2d87e2d
Amaury Carrade <amaury@carrade.eu>
2018-08-16 00:45:06 +0200

    Added options to disable automatic sync on save/delete/media.
    If disabled, sync will not occur when pages are saved, deleted, or medias uploaded/removed (according to the selected options). This could be used to fix performances issues due to the git pull/push
    being executed synchronously when pages are saved, implying way too long loading times. But sync must be done manally, or using a cron, if auto sync is disabled (obviously).
    
    Fixes #51.

--- git-sync.php
+++ git-sync.php
@@ -47,10 +47,10 @@ class GitSyncPlugin extends Plugin
                 'onAdminMenu'          => ['onAdminMenu', 0],
                 'onAdminSave'          => ['onAdminSave', 0],
                 'onAdminAfterSave'     => ['onAdminAfterSave', 0],
-                'onAdminAfterSaveAs'   => ['synchronize', 0],
-                'onAdminAfterDelete'   => ['synchronize', 0],
-                'onAdminAfterAddMedia' => ['synchronize', 0],
-                'onAdminAfterDelMedia' => ['synchronize', 0],
+                'onAdminAfterSaveAs'   => ['onAdminAfterSaveAs', 0],
+                'onAdminAfterDelete'   => ['onAdminAfterDelete', 0],
+                'onAdminAfterAddMedia' => ['onAdminAfterMedia', 0],
+                'onAdminAfterDelMedia' => ['onAdminAfterMedia', 0],
             ]);
 
             return;
@@ -186,8 +186,6 @@ class GitSyncPlugin extends Plugin
             $this->controller->execute();
             $this->controller->redirect();
         }
-
-
     }
 
     public function onAdminSave($event)
@@ -221,6 +219,10 @@ class GitSyncPlugin extends Plugin
 
     public function onAdminAfterSave($event)
     {
+        if (!$this->grav['config']->get('plugins.git-sync.sync.on_save', true)) {
+            return true;
+        }
+
         $obj           = $event['object'];
         $isPluginRoute = $this->grav['uri']->path() == '/admin/plugins/' . $this->name;
 
@@ -247,6 +249,33 @@ class GitSyncPlugin extends Plugin
         }
 
         $this->synchronize();
+
+        return true;
+    }
+
+    public function onAdminAfterSaveAs()
+    {
+        if ($this->grav['config']->get('plugins.git-sync.sync.on_save', true)) {
+            $this->synchronize();
+        }
+
+        return true;
+    }
+
+    public function onAdminAfterDelete()
+    {
+        if ($this->grav['config']->get('plugins.git-sync.sync.on_delete', true)) {
+            $this->synchronize();
+        }
+
+        return true;
+    }
+
+    public function onAdminAfterMedia()
+    {
+        if ($this->grav['config']->get('plugins.git-sync.sync.on_media', true)) {
+            $this->synchronize();
+        }
 
         return true;
     }
